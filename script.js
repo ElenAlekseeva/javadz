529b28fa3645e06291c46729ce6b6c8b3fd8e045
Elena <lady.aleck98@icloud.com>
2023-02-06 19:50:23 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Задание 1");
+let celsius= 3;
+let result = (9 / 5) * celsius + 32;
+console.log(result);
+
+console.log("Задание 2");
+let names = "Elena";
+let admin = "Администратор:" + names;
+console.log(admin);
