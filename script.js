5ea98b28da4023af806ed29e47e7ae7fd2858d49
Elena <lady.aleck98@icloud.com>
2023-02-15 12:24:30 +0300

    дз 3

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ console.log("Задание 2");
 let names = "Elena";
 let admin = "Администратор:" + names;
 console.log(admin);
+
+console.log("Дз 3 . Задание 1")
+let number = parseInt(prompt("Введите число"))
+function cube(num) 
+{result = Math.pow(num, 3)
+return console.log(`${num}^3=${result}`)
+}
+cube(number)
+
+console.log("Дз 3. Задание 2")
+function salary() {
+ let userSalary = parseInt(prompt("Напишите вашу зарплату"))
+ if (isFinite(userSalary) === true){
+ let result = userSalary - userSalary * 0.13
+ return console.log(`Ваша зарплата после вычета налога равна ${result}`)
+ }
+ return console.log(`Ошибка ${userSalary}`)
+ }
+salary()
+
+console.log("Дз 3. Задание 3")
+let num1 = parseInt(prompt("Введите первое число"))
+let num2 = parseInt(prompt("Введите второе число"))
+let num3 = parseInt(prompt("Введите третье число"))
+
+function compareNumber(num1, num2, num3) {
+let max = Math.max(num1, num2, num3)
+console.log(`Большее число ${max}`)
+}
+compareNumber(num1, num2, num3)
